0ad513c759fe7d0fa2ef6affc7c2c19e2236928e
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-17 17:38:29 -0300

    agrego vue y login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Order;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Create a new controller instance.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
      */
-    public function real_time_orders()
+    public function __construct()
     {
-        return view('orders.realtime');
+        $this->middleware('auth');
     }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('/orders/real_time', function () {
     return view('orders.realtime');
 });
 
-require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
