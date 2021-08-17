5972c05ad6baefd107b5532a43665202e1574c64
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-17 11:51:56 -0300

    Revert login

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,4 @@ Route::get('/orders/real_time', function () {
     return view('orders.realtime');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
