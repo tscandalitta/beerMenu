cdaa9aa7c938815b63ddd33244de9156a7686111
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-17 10:58:10 -0300

    elimino scripts innecesarios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,10 +26,4 @@
             </main>
         </div>
     </body>
-
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js">
-    </script>
-    <script type="text/javascript"
-            src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </html>
