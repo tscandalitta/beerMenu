d39d811f1f8197dcc27cd8c65b9babba402b87be
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-17 10:58:20 -0300

    Revert "install vue"
    This reverts commit 40fedf56ad9a65f105d2420bb5c9571e38170a98.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
 require('./bootstrap');
 
 require('alpinejs');
-
-import { createApp } from 'vue';
-import OrdenComponent from  './components/OrdenComponent'
-
-createApp({
-    components: {
-        OrdenComponent,
-    }
-}).mount('#app');
