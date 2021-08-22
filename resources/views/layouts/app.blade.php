f63f60ce943dd315ca7dc82188c34548e4ea86c1
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-22 16:53:49 -0300

    agrego logo a la app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <!-- Icon -->
+    <link rel="icon" href="/assets/logo.png" type="image/x-icon">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
