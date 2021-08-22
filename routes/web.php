a81a3340d92945ff34c43e63287f811c8ba0813a
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-22 02:00:33 -0300

    agrego ruta para generar codigos QR

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,11 @@ Route::get('/orders/real_time', [OrderController::class, 'realTime']);
 Route::get('/tables', [TableController::class,'showTables']);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/qrcode', function () {
+    return QrCode::size(300)
+        ->generate(env('APP_URL') . '/?table=' . request('id') . '&token=' . request('token'));
+});
 
 Auth::routes();
 
