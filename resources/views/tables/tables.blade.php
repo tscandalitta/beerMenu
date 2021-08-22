d415fd41ccde4461b817654f5c4c43217448f1d9
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-22 02:01:30 -0300

    uso componente de qrs

--- resources/views/tables/tables.blade.php
+++ resources/views/tables/tables.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($tables as $table)
-        {!! QrCode::size(300)->generate('https://maxituyhorus.com/?table_id=' . $table->id . '&token=' . $table->token); !!}
-    @endforeach
-
+    <tables tables={{ $tables }}></tables>
 @endsection
-
