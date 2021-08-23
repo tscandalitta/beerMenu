7f3c8841893575c8a14d331e31a7d11a8c2a303a
Tomas Scandalitta <38343557+tscandalitta@users.noreply.github.com>
2021-08-22 21:10:13 -0300

    logica para crear las solicitudes

--- app/Http/Controllers/AtentionRequestController.php
+++ app/Http/Controllers/AtentionRequestController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\AtentionRequest;
+use App\Models\Order;
+use App\Models\Table;
 
 class AtentionRequestController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(AtentionRequest::all());
+    }
+
+    public function store()
+    {
+        $table = Table::find(request("table"));
+        if($table->token == request("token")) {
+            $atentionRequest = AtentionRequest::create([
+                'table_id' => $table->id,
+                'type' => request('type'),
+                'comments' => request('comments'),
+            ]);
+            $atentionRequest->save();
+            return response()->json($atentionRequest);
+        }
+        return response()->json(["msg" => "Unauthorized"], 401);
+
+    }
 }
